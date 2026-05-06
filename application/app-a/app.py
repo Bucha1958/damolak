8a61cd104cdcd86f1dfc060d2f1d65f468939c8b
Stanley Okorie <okoriemmadu@gmail.com>
2026-05-06 19:28:47 +0100

    fix the port mapping

--- application/app-a/app.py
+++ application/app-a/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    b = requests.get("http://app-b:5000").text
+    b = requests.get("http://app-b").text
     return f"App A here -> {b}"
 
 if __name__ == "__main__":
--- application/app-b/app.py
+++ application/app-b/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    c = requests.get("http://app-c:5000").text
+    c = requests.get("http://app-c").text
     return f"App B here -> {c}"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
